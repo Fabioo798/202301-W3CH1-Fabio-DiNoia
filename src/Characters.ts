3a43ce3e75514bdde7273cb6430bd0ae2006eff3
Fabio <fabiodn.798@gmail.com>
2023-01-30 23:25:56 +0100

    create personajes array

--- src/Characters.ts
+++ src/Characters.ts
@@ -2,9 +2,13 @@ import { Advisor } from './Asesor';
 import { King } from './Rey';
 import { Fighter } from './Luchador';
 import { Squire } from './escudero';
-import { Got } from './Character';
 
 
-export const characters = [
-  Joffrey Baratheon
-]
+
+export const characters: Any[] = [];
+
+characters.push(new King('Joffrey Baratheon', 'Lannister', 12, true, 1));
+characters.push(new Fighter('Jaime Lannister', 'Lannister', 20, true, 'Sword', 9));
+characters.push(new Fighter('Daenerys Targaryen', 'Targaryan', 20, true, 'fire', 3 ));
+characters.push(new Advisor('Tyrion Lannister', 'Lannister', 20, true, 'Daenerys' ));
+characters.push(new Squire('Bronn', 'High Garden', 30, true, 'Jaime Lannister', 9));
